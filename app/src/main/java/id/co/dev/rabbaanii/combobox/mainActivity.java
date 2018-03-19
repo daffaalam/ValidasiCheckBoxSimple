27f46c836c7ae7ff303eff94879b4f13defa594a
Abiyyu Daffa' Alam <daffaalam@gmail.com>
2018-03-19 22:24:33 +0700

    update 19.03.18

--- app/src/main/java/id/co/dev/rabbaanii/combobox/mainActivity.java
+++ app/src/main/java/id/co/dev/rabbaanii/combobox/mainActivity.java
@@ -1,5 +1,6 @@
 package id.co.dev.rabbaanii.combobox;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,35 +10,44 @@ import android.widget.Toast;
 
 public class mainActivity extends AppCompatActivity {
 
+    TextView texta, textb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        texta = (TextView) findViewById(R.id.piliha);
+        textb = (TextView) findViewById(R.id.pilihb);
     }
 
+    @SuppressLint("SetTextI18n")
     public void onCheckboxClicked(View view) {
         boolean checked = ((CheckBox) view).isChecked();
-        TextView text = (TextView)findViewById(R.id.pilih);
-        switch(view.getId()) {
+
+        switch (view.getId()) {
+
             case R.id.checkbox_meat:
-                if (checked){
+                if (checked) {
                     Toast.makeText(mainActivity.this, "This Meat!", Toast.LENGTH_SHORT).show();
-                    text.setText("Meat");
-                }if(!checked){
-                    Toast.makeText(mainActivity.this, "This Unchecked!", Toast.LENGTH_SHORT).show();
-                    text.setText("");
+                    texta.setText("Meat");
+                }
+                if (!checked) {
+                    Toast.makeText(mainActivity.this, "Meat Unchecked!", Toast.LENGTH_SHORT).show();
+                    texta.setText("");
                 }
                 break;
+
             case R.id.checkbox_cheese:
-                if (checked){
+                if (checked) {
                     Toast.makeText(mainActivity.this, "This Cheese!", Toast.LENGTH_SHORT).show();
-                    text.setText("Cheese");
-                }if(!checked){
-                    Toast.makeText(mainActivity.this, "This Unchecked!", Toast.LENGTH_SHORT).show();
-                    text.setText("");
+                    textb.setText("Cheese");
+                }
+                if (!checked) {
+                    Toast.makeText(mainActivity.this, "Cheese Unchecked!", Toast.LENGTH_SHORT).show();
+                    textb.setText("");
                 }
                 break;
         }
     }
-
 }
